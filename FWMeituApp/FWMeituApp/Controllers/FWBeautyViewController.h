6dc0a82bac2cd5e48ec56600ead2c3ce37b0f65c
ForrestWoo <wjqlxt@163.com>
2015-09-19 16:01:53 +0800

    111

---
+++ FWMeituApp/FWMeituApp/Controllers/FWBeautyViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWBeautyViewController.h
+//  FWMeituApp
+//
+//  Created by ForrestWoo on 15-9-19.
+//  Copyright (c) 2015年 ForrestWoo co,.ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWBeautyViewController : UIViewController
+
+@end
